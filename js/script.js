ee1c2789a7d13df0b16cfc2a7fe1cebceaa118b1
Katherine Love <270010932@bun20-l1-ph03.acg.ac.nz>
2019-05-02 09:49:01 +1200

    Javascript Lesson

---
+++ js/script.js
@@ -0,0 +1,17 @@
+//console.log('Javascript is loaded');
+
+//alert("computer says no");
+
+//console.log(   (1 * 2)  - (" string data type")   );
+
+//console.log("string 1" + " string 2");
+
+//var variable1 = "this is now a string";
+
+//console.log(variable1);
+
+var question = prompt("What is your name?");
+var secondQuestion = prompt("What is your favourite colour?");
+console.log(question);
+console.log(secondQuestion);
+console.log("Hello " + question + " your favourite colour is " + secondQuestion);
